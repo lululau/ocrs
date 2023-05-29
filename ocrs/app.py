2a7710224ef0851b40b9d75ae440d07e8007379c
Liu Xiang <liuxiang@ktjr.com>
2023-05-29 15:04:34 +0800

    Initial commit

---
+++ ocrs/app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import base64
+from logging.config import dictConfig
+
+import ddddocr
+from flask import Flask, request
+
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
+
+
+app = Flask(__name__)
+
+ocr_engine = ddddocr.DdddOcr()
+
+@app.route("/ocr", methods=["POST"])
+def ocr():
+    app.logger.info("ocr request received, image size: %d", len(request.json["image_base64"]))
+    image_base64 = request.json["image_base64"]
+    image_bytes = base64.b64decode(image_base64.encode("utf-8"))
+    text = ocr_engine.classification(image_bytes)
+    app.logger.info("ocr result: %s", text)
+    return {"text": f"{text}"}
